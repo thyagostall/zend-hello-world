e4d82325bf308a5a1f4769e34867303ff004d120
thyagostall <thstall@gmail.com>
2017-02-19 18:58:59 -0300

    One more try...

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -5,7 +5,9 @@ return [
         'connection' => [
             'orm_default' => [
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-                'url' => $_ENV['JAWSDB_URL'],
+                'params' => [
+                    'url' => $_ENV['JAWSDB_URL'],
+                ]
             ]
         ]
     ]
