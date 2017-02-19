d71650d52aa043d09f54b622b072a7cd95d50075
thyagostall <thstall@gmail.com>
2017-02-19 18:56:01 -0300

    Doctrine global for mysql

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'url' => $_ENV['JAWSDB_URL'],
+            ]
+        ]
+    ]
+];
